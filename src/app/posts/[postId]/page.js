338966e235f819086a63d48e8eb3f3e90f5665dd
amirabbas <amirabbasghare5235@gmail.com>
2024-09-28 09:24:48 +0330

    feat : create PostPage

--- src/app/posts/[postId]/page.js
+++ src/app/posts/[postId]/page.js
@@ -1,8 +1,34 @@
+import { api } from '@/configs/api'
 import React from 'react'
 
-function PostPage() {
+async function PostPage({params}) {
+    const PostDetail=await api.get(`posts/${params.postId}`)
+    const Comments=await api.get('comments')
+    const CommentsPost=Comments.data.filter(comment => comment.postId === +params.postId)
+    console.log(CommentsPost);
   return (
-    <div>PostPage</div>
+    <div style={{marginInline:"40px",marginTop:'20px'}}>
+        <p>PostPage</p>
+
+        <h1>Comments</h1>
+        <hr style={{marginInline:'20px',marginTop:'30px'}} />
+        {CommentsPost.map(comment =>(
+            <div style={{marginTop:"10px",marginBottom:"10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:'space-between'}}>
+                     <p>
+                       <span style={{backgroundColor:'green'}}>Name  </span>: {comment.name}
+                    </p>
+                    <p>
+                       <span style={{backgroundColor :'yellow'}}> Email  </span>: {comment.email}
+                    </p>
+                </div>
+                <p><span style={{backgroundColor:'red'}}>Text</span> : {comment.body}</p>
+                <hr style={{marginTop:'10px'}}/>
+            </div>
+        ))}
+        
+        
+    </div>
   )
 }
 
