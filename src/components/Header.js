7d50ea33cb231b8f9914ae26dce0df67b30d96fe
amirabbas <amirabbasghare5235@gmail.com>
2024-10-01 11:23:05 +0330

    refactor : socket.io added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 // src/components/Header.js
-'use client'
+"use client";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./LanguageSwitcher";
@@ -7,20 +7,34 @@ import LanguageSwitcher from "./LanguageSwitcher";
 const Header = () => {
   const { t } = useTranslation();
   return (
-    <header style={{paddingInline:'40px',paddingTop:"20px",paddingBottom:"20px",backgroundColor:'green',color:"white"}}>
-      <nav style={{display:'flex',justifyContent: 'space-between',alignItems:'center'}}>
+    <header
+      style={{
+        paddingInline: "40px",
+        paddingTop: "20px",
+        paddingBottom: "20px",
+        backgroundColor: "green",
+        color: "white",
+      }}
+    >
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <div>
-          <Link href="/users">{t('users')}</Link>
+          <Link href="/users">{t("users")}</Link>
         </div>
         <div>
-          <Link href="/albums">{t('albums')}</Link>
+          <Link href="/albums">{t("albums")}</Link>
         </div>
         <div>
-          <Link href="/posts">{t('posts')}</Link>
+          <Link href="/posts">{t("posts")}</Link>
         </div>
       </nav>
-      <div style={{marginTop:10}}>
-            <LanguageSwitcher/>
+      <div style={{ marginTop: 10 }}>
+        <LanguageSwitcher />
       </div>
     </header>
   );
